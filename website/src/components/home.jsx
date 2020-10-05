82083a2e224d6b993824977726be3257f133eb16
Kevin Ly <45864296+k-ly73@users.noreply.github.com>
2020-10-04 22:18:33 -0700

    Added packages

---
+++ website/src/components/home.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import '../assets/stylesheet.css';
+import NavBar from './navigation.jsx';
+class HomePage extends Component {
+    render(){
+        return (
+            <body>
+                <NavBar></NavBar>
+                <div>
+                    
+                </div>
+            </body>
+        )
+           
+        
+    }
+}
+export default HomePage
---
+++ website/src/components/navigation.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+import '../assets/nav.css';
+
+class NavBar extends Component {
+    render() {
+        return(
+            <nav>
+                <ul id = "nav">
+                    <li>
+                        <a href>Home</a>
+                    </li>
+                    <li>
+                        <a href>About</a>
+                    </li>
+                    <li>
+                        
+                    </li>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+}
+export default NavBar
